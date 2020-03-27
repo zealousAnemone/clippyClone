7858042196a12fdff5d11aa6999e31ba34fefb24
Rebecca Hanan <beckett.hanan@gmail.com>
2020-03-26 20:07:05 -0700

    got clippy to appear yaaaaay

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log('testing manifest');
+// check if the page is loaded
+window.onload = function () {
+  console.log('I\'m done!');
+  // create a container for Clippy Cat
+  const clippyBox = document.createElement('div');
+  // add styling
+  clippyBox.classList.add('clippyContainer');
+
+  // create our Clippy Cat!
+  const clippy = document.createElement('img');
+  clippy.src = 'https://i.ibb.co/XSJ854n/clippy-cat.png';
+
+  // put Clippy Cat in its box!
+  clippyBox.append(clippy);
+
+  // load the image on the page inside the div
+  document.body.append(clippyBox);
+};
